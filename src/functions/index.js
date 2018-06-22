b36aebb44b17fbdbb9dfb6d7a1a115865c4717d2
ext_tsaiyouy <tsaiyou_yuan@trend.com.tw>
2018-06-22 18:22:15 +0800

    intergrate firestore with function

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,8 +1,46 @@
-const functions = require('firebase-functions');
+import admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import express from 'express';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('root');
+});
+
+app.post('/product', async (req, res) => {
+    const { body } = req;
+    if (body) {
+        try {
+            const doc = await db.collection('products').add({ ...body });
+            const docSnapshot = await doc.get();
+            const data = docSnapshot.data();
+            res.status(200).send(data);
+        } catch(e) {
+            console.error(e);
+            res.status(500).send(e);
+        }
+    }
+});
+
+app.get('/product/all', async (req, res) => {
+    try {
+        const querySnapshot = await db.collection('products').get();
+        const data = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+        res.status(200).send(data);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send(e);
+    }
+});
+
+const api = functions.https.onRequest(app);
+
+export { api };
